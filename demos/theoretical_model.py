91bf647e54eb4be08d754142b0d58e7cca37e236
josephb <josephb@stradigi.ai>
2022-02-01 13:46:34 -0500

    try app

--- demos/theoretical_model.py
+++ demos/theoretical_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 
 from lime.lime_tabular import LimeTabularExplainer
 
@@ -23,32 +24,7 @@ def local_approximation(
     return y
 
 
-if __name__ == "__main__":
-    train_df = pd.DataFrame(
-        np.random.uniform(low=-2.5, high=2.5, size=(10000, 2)),
-        columns=["Feature 1", "Feature 2"],
-    )
-
-    explainer = LimeTabularExplainer(
-        train_df.values,
-        mode="regression",
-        feature_names=train_df.columns,
-        discretize_continuous=False,
-        sample_around_instance=True,
-    )
-
-    instance_to_be_explained = pd.DataFrame(
-        {
-            "Feature 1": [-0.5],
-            "Feature 2": [0.2],
-        }
-    )
-
-    explanation = explainer.explain_instance(
-        instance_to_be_explained.values.ravel(), fitted_model_prediction
-    )
-    print(explanation.as_list())
-
+def plot_model_and_local_preds(train_df, instance_to_be_explained):
     x = train_df.values[:, 0]
     y_pred = fitted_model_prediction(train_df.values)
 
@@ -56,12 +32,47 @@ if __name__ == "__main__":
     y_approx = local_approximation(
         local_data, approx_at=instance_to_be_explained.values
     )
-    plt.scatter(x=x, y=y_pred, marker=".")
-    plt.scatter(x=local_data[:, 0], y=y_approx, marker=".")
-    plt.scatter(
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+
+    ax.scatter(x=x, y=y_pred, marker=".")
+    ax.scatter(x=local_data[:, 0], y=y_approx, marker=".")
+    ax.scatter(
         x=instance_to_be_explained.values[:, 0],
         y=fitted_model_prediction(instance_to_be_explained.values),
         marker="*",
         s=200,
     )
-    plt.show()
+    return fig
+
+
+st.title('Theoretical Model')
+
+train_df = pd.DataFrame(
+    np.random.uniform(low=-2.5, high=2.5, size=(10000, 2)),
+    columns=["Feature 1", "Feature 2"],
+)
+
+explainer = LimeTabularExplainer(
+    train_df.values,
+    mode="regression",
+    feature_names=train_df.columns,
+    discretize_continuous=False,
+    sample_around_instance=True,
+)
+
+instance_to_be_explained = pd.DataFrame(
+    {
+        "Feature 1": [-0.5],
+        "Feature 2": [0.2],
+    }
+)
+
+explanation = explainer.explain_instance(
+    instance_to_be_explained.values.ravel(), fitted_model_prediction
+)
+print(explanation.as_list())
+
+
+fig = plot_model_and_local_preds(train_df, instance_to_be_explained)
+st.pyplot(fig)
