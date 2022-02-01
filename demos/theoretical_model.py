56f37ad8194be4dd605b4a6696aa4ae03444ddde
josephb <josephb@stradigi.ai>
2022-02-01 14:11:28 -0500

    plot in app

--- demos/theoretical_model.py
+++ demos/theoretical_model.py
@@ -28,7 +28,7 @@ def plot_model_and_local_preds(train_df, instance_to_be_explained):
     x = train_df.values[:, 0]
     y_pred = fitted_model_prediction(train_df.values)
 
-    local_data = train_df.values / 5 + instance_to_be_explained.values
+    local_data = train_df.values / 6 + instance_to_be_explained.values
     y_approx = local_approximation(
         local_data, approx_at=instance_to_be_explained.values
     )
@@ -43,36 +43,43 @@ def plot_model_and_local_preds(train_df, instance_to_be_explained):
         marker="*",
         s=200,
     )
+    ax.set_ylim(0, 1.2)
     return fig
 
 
-st.title('Theoretical Model')
+def local_interpret(train_df, instance_to_be_explained):
+    explainer = LimeTabularExplainer(
+        train_df.values,
+        mode="regression",
+        feature_names=train_df.columns,
+        discretize_continuous=False,
+        sample_around_instance=True,
+    )
+    explanation = explainer.explain_instance(
+        instance_to_be_explained.values.ravel(), fitted_model_prediction
+    )
+    return explanation
+
+
+# --------------------------------- APP ----------------------------------------
+st.title("Theoretical Model")
+st.sidebar.markdown("Get feature importance for:")
+f1 = st.sidebar.slider("Feature 1", -2.5, 2.5, 0.0)
+f2 = st.sidebar.slider("Feature 2", -2.5, 2.5, 0.0)
 
 train_df = pd.DataFrame(
     np.random.uniform(low=-2.5, high=2.5, size=(10000, 2)),
     columns=["Feature 1", "Feature 2"],
 )
 
-explainer = LimeTabularExplainer(
-    train_df.values,
-    mode="regression",
-    feature_names=train_df.columns,
-    discretize_continuous=False,
-    sample_around_instance=True,
-)
-
 instance_to_be_explained = pd.DataFrame(
     {
-        "Feature 1": [-0.5],
-        "Feature 2": [0.2],
+        "Feature 1": [f1],
+        "Feature 2": [f2],
     }
 )
 
-explanation = explainer.explain_instance(
-    instance_to_be_explained.values.ravel(), fitted_model_prediction
-)
-print(explanation.as_list())
-
+# print(explanation.as_list())
 
 fig = plot_model_and_local_preds(train_df, instance_to_be_explained)
 st.pyplot(fig)
