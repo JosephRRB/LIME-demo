2cebb5a9785910734d0f2e30a8f357899f848250
josephb <josephb@stradigi.ai>
2022-02-03 17:18:57 -0500

    start sklearn models writeup

--- demos/sklearn_models.py
+++ demos/sklearn_models.py
@@ -15,7 +15,7 @@ from fit_sklearn_models import fit_models
 
 def calculate_test_score(model, X_test, y_test):
     model_test_score = model.score(X_test, y_test)
-    round_percent = round(model_test_score * 100, 1)
+    round_percent = round(model_test_score * 100, 2)
     return f"{round_percent} %"
 
 
@@ -62,7 +62,7 @@ def plot_feature_importance_for_clf_model(
 
     fig, ax = plt.subplots(1, 1, figsize=figsize)
 
-    fi_data = pd.DataFrame(*fi.values())
+    fi_data = pd.DataFrame(*fi.values())  # .sort_values(by=0)
     positive_fi = fi_data.iloc[:, 1] > 0
     color = positive_fi.map({True: "blue", False: "red"})
 
@@ -80,10 +80,31 @@ def plot_feature_importance_for_clf_model(
 
 
 def deploy_plots_for_sklearn_models():
-    # https://archive.ics.uci.edu/ml/datasets/banknote+authentication
-    # https://jamesmccaffrey.wordpress.com/2020/08/18/in-the-banknote-authentication-dataset-class-0-is-genuine-authentic/
+    st.header("Scikit-learn Models")
+    st.markdown(
+        """
+    Now, we can consider explaining the predictions of different models from 
+    scikit-learn. Namely, they are: 
+    - [Support Vector Machines](https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html),
+    - [K-Nearest Neighbors](https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html),
+    - [Random Forests](https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html),
+    - and [Fully-Connected Neural Networks](https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html)
+    
+    We will be using the [banknote authentication dataset](https://archive.ics.uci.edu/ml/datasets/banknote+authentication)
+    for binary classification. The dataset contains four continuous numerical 
+    features extracted from the image of the banknotes and its 
+    wavelet-transformed version. The class labels are also balanced and their
+    meanings are clarified [here](https://jamesmccaffrey.wordpress.com/2020/08/18/in-the-banknote-authentication-dataset-class-0-is-genuine-authentic/).
+    
+    The classification models have already been fitted on a certain split of the 
+    data, but can be refitted by clicking the button below. This will create a 
+    new train and test data split and the models will be fitted accordingly.
+    The new split and new models will then be used in the remainder of the demo.
+    After clicking the refit button, there will also be an option to take a look
+    at the basic training code used.  
+    """
+    )
     data_dir = Path(__file__).parents[1] / "demos/data"
-
     if st.button("Refit models on new data split"):
         fit_models()
         st.write("Refitted!")
@@ -103,7 +124,7 @@ def deploy_plots_for_sklearn_models():
         "Support Vector Machine Classifier",
         "K-Nearest Neighbor Classifier",
         "Random Forest Classifier",
-        "Fully-Connected Neural Network Classifier"
+        "Fully-Connected Neural Network Classifier",
     ]
     models = [svc, knn, rfc, mlp]
     for name, model in zip(model_names, models):
@@ -112,6 +133,18 @@ def deploy_plots_for_sklearn_models():
             "Test Accuracy", calculate_test_score(model, X_test, y_test)
         )
 
+    st.markdown(
+        """
+    Along the sidebar, the accuracy of the four models on the test set are 
+    displayed.
+    """
+    )
+    st.header("Explaining an instance")
+    st.markdown(
+        """
+    We can now pick an instance from the test dataset 
+    """
+    )
     st.dataframe(X_test)
     query_idx = st.number_input(
         "Select a row index:", min_value=0, max_value=len(X_test)
